80496a40a4e135573869958815af7c7a8edf1371
janitha <janithapraneeth23@gmail.com>
2022-10-07 12:37:48 +0530

    Init

---
+++ src/main/java/com/janitha/videoenhancer/serverAtCloud/ServerAtCloudApplication.java
@@ -0,0 +1,13 @@
+package com.janitha.videoenhancer.serverAtCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAtCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAtCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janitha/videoenhancer/serverAtCloud/ServerAtCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janitha.videoenhancer.serverAtCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAtCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
